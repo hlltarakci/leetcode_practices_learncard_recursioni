4d7f73784c8726ca785e3039c1f19b9a599ec788
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-16 18:09:21 +0300

    Create pascalsTriangleII.java

---
+++ code/pascalsTriangleII.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/explore/learn/card/recursion-i/251/scenario-i-recurrence-relation/3234/
+
+class Solution {
+    /*
+        time: O(k^2)
+        space: O(k)
+    */
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> prev = new ArrayList<>();
+        for(int i=0; i<=rowIndex; i++) {
+            result = new ArrayList<>();
+            result.add(1);
+            
+            for(int j=0; j<prev.size()-1; j++) 
+                result.add(prev.get(j) + prev.get(j+1));
+            
+            if(i > 0) result.add(1);
+            prev = result;
+        }
+        
+        return result;
+    }
+}
